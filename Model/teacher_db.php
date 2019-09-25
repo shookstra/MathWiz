33da405415bd3177ee901dbce7af3721c30aa180
William Remaklus <wr6964@southeast.edu>
2019-09-25 15:49:59 -0500

    Merge origin/master
    Conflicts:
    	TeamSDB.sql

--- Model/teacher_db.php
+++ Model/teacher_db.php
@@ -1,22 +1,117 @@
 <?php
 
+        teacher;
 
+        $teacherID;
+	$password;
+	$fName;
+	$lName;
+
+        teacherID;
+	password;
+	fName;
+	lName;
+        
 class teacher_db {
-    public static function validate_teacher_login($teacherID) {
+    
+    public static function select_all() {
+        $db = Database::getDB();
+
+        $queryUsers = 'SELECT * FROM teacher';
+        $statement = $db->prepare($queryUsers);
+        $statement->execute();
+        $rows = $statement->fetchAll();
+        $teacher = [];
+
+        foreach ($rows as $value) {
+            $teacher[$value['teacherID']] = new student($value['teacherID'], $value['password'], $value['$fName'], $value['lName']);
+        }
+        $statement->closeCursor();
+
+        return $teacher;
+    }
+	
+	
+	
+public static function get_teacher_by_id($teacherID) {
         $db = Database::getDB();
         $query = 'SELECT *
-              FROM student
-              WHERE studentID= :studentID';
+              FROM teacher
+              WHERE teacherID= :teacherID';
 
         $statement = $db->prepare($query);
-        $statement->bindValue(':studentID', $studentID);
+        $statement->bindValue(':teacherID', $teacherID);
         $statement->execute();
         $value = $statement->fetch();
         
-        $theStudent = new student($value['studentID'], $value['studentFName'], $value['studentLName'], $value['studentAddLevel'], $value['studentSubLevel'], $value['studentMultLevel'], $value['studentDivLevel'], $value['studentPWord'], $value['teacherID']);
+        $teacher = new teacher($value['teacherID'], $value['password'], $value['$fName'], $value['lName']);
+        
+        $statement->closeCursor();
+
+        return $teacher;
+    }
+	
+	
+	
+	
+	
+public static function update_teacher($teacherID, $password, $fName, $lName) {
+        
+
+        $db = Database::getDB();
+        $query = $query = 'UPDATE teacher
+              SET fName = :fName,
+                  lName = :studentLName,
+                  password = :lName,
+                WHERE teacherID = :teacherID';
+        try {
+            $statement = $db->prepare($query);
+            $statement->bindValue(':password', $password);
+            $statement->bindValue(':fName', $fName);
+            $statement->bindValue(':lName', $lName);
+            $statement->bindValue(':teacherID', $teacherID);
+            $row_count = $statement->execute();
+            $statement->closeCursor();
+            return $row_count;
+        } catch (PDOException $e) {
+            $error_message = $e->getMessage();
+            display_db_error($error_message);
+        }
+    }
+	
+	
+	
+public static function delete_by_ID($teacherID) {
+        $db = Database::getDB();
+        $query = 'DELETE from teacher WHERE teacherID= :teacherID';
+        try {
+            $statement = $db->prepare($query);
+            $statement->bindValue(':teacherID', $teacherID);
+            $row_count = $statement->execute();
+            $statement->closeCursor();
+            return $row_count;
+        } catch (PDOException $e) {
+            $error_message = $e->getMessage();
+            display_db_error($error_message);
+        }
+    }
+    
+    
+    public static function validate_teacher_login($teacherID) {
+        $db = Database::getDB();
+        $query = 'SELECT *
+              FROM teacher
+              WHERE teacherID= :teacherID';
+
+        $statement = $db->prepare($query);
+        $statement->bindValue(':teacherID', $teacherID);
+        $statement->execute();
+        $value = $statement->fetch();
+        
+        $theTeacher = new teacher($value['teacherID'], $value['password'], $value['fName'], $value['lName']);
 
         $statement->closeCursor();
 
-        return $theStudent;
+        return $theTeacher;
     }
 }
