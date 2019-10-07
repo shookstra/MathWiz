f2483792c03e46928b8ca1dcab27637f724b8c43
kurti <kurti@10.17.44.69>
2019-10-07 13:59:35 -0500

    added pages to update password, create user, and confirmation pages for new user, password changed. Added to index, still need the logic behind the create/update.

---
+++ View/passwordChanged.php
@@ -0,0 +1,22 @@
+<?php include '/xampp/htdocs/mathwiz/view/header.php'; ?>
+
+    <body>
+        <div id="wrapper">
+    <div id="heading"> 
+        <h1>Math Drill Site</h1>
+    </div>
+            <?php include '/xampp/htdocs/mathwiz/view/nav.php'; ?>
+           
+            <div class="content"> 
+                 <h2>Success!</h2>
+                 <div class="content">
+                     <p>Password successfully updated</p>
+                 </div>
+        
+        
+        
+        </div>
+        
+        
+<?php include '/xampp/htdocs/mathwiz/view/footer.php'; ?>
+
