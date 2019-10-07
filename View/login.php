185980ae20fc40a75756bd68ec71ab43b37c8e0c
sh615576 <sh615576@LR00114.southeast.edu>
2019-10-07 14:43:46 -0500

    Updated styling. Updated logic.

--- View/login.php
+++ View/login.php
@@ -1,31 +1,30 @@
 <?php include 'view/header.php'; ?>
 
 <div id="wrapper">
-
-    <div id="heading"> 
-        <h1>Math Drill Site</h1>
-    </div>
+    <?php include 'view/headerElement.php'; ?>
     <?php include 'view/nav.php'; ?>
 
-    <h2>Login</h2>
-    <p>Please login. If are having difficulty please contact your teacher. To reset password, please contact admin.</p>
-    <div  id="login">
 
-        <form action="index.php" method="POST">
-            <input type="hidden" name="action" value="loggedin">
-            <select name='userType'>
-                <option value="student" selected>Student</option>
-                <option value="teacher">Teacher</option>
-                <option value="admin">Administrator</option>
-            </select><br>
-            <label>User ID: </label><input class="entry" type="text" name="userID"><br><br>
-            <label>Password: </label><input class="entry" type="password" name="password"><br><br>
+    <div class="content">
+        <h2>Login</h2>
+        <p>Please login. If you are having difficulty please contact your teacher. To reset password, please contact admin.</p>
+
+        <div id="login">
             <form action="index.php" method="POST">
-                <input type="hidden" value="loggedin" name="action">
-                <input type="submit" value="Login">
-            </form>
-            <p><?php echo $errors ; ?></p>
+                <input type="hidden" name="action" value="loggedin">
+                <select name='userType'>
+                    <option value="student" selected>Student</option>
+                    <option value="teacher">Teacher</option>
+                    <option value="admin">Administrator</option>
+                </select><br>
+                <label>User ID: </label><input class="entry" type="text" name="userID"><br><br>
+                <label>Password: </label><input class="entry" type="password" name="password"><br><br>
+                <form action="index.php" method="POST">
+                    <input type="hidden" value="loggedin" name="action">
+                    <input type="submit" value="Login">
+                </form>
+                <p><?php echo $errors; ?></p>
+        </div>
 
-
-            </div>
-            <?php include 'view/footer.php'; ?>
+    </div>
+    <?php include 'view/footer.php'; ?>
--- View/nav.php
+++ View/nav.php
@@ -1,6 +1,4 @@
-<?php // echo var_dump($_SESSION)            ?>
 <div id="nav">
-
     <table>
         <tr>
             <td>
